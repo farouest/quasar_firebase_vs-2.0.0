d43cc53b7cc70f7fc3908fe14d13283bbb5945c6
Arno Paris <arnaud.courrot@gmail.com>
2020-11-07 18:24:58 +0100

    news commit test modification

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -57,12 +57,12 @@ export default {
         return {
             leftDrawerOpen: this.$q.platform.is.desktop,
             navs: [{
-                    label: 'Todo',
+                    label: 'Vos infos perso',
                     icon: 'list',
                     to: '/'
                 },
                 {
-                    label: 'Settings',
+                    label: 'Paramètre',
                     icon: 'settings',
                     to: '/settings'
                 }
